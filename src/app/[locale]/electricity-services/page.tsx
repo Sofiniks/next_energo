e1061c0bc75525f1a0e55e90bf6d1a8315ba4de9
Sofiniks <sofija.nikitina@inbox.lv>
2023-10-11 11:15:06 +0300

    metadata

--- src/app/[locale]/electricity-services/page.tsx
+++ src/app/[locale]/electricity-services/page.tsx
@@ -1,13 +1,25 @@
-import { Metadata } from 'next';
 import StatisticBanner from '@/components/banners/StatisticBanner';
 import ContactForm from '@/components/contactForm/ContactForm';
 import HeroSection from '@/components/sections/HeroSection';
 import ServicesSection from '@/components/sections/ServicesSection';
 import ServicesData from '@/data/electricityServicesData.json';
 
-export const metadata: Metadata = {
-  title: 'Pakalpojumi',
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: {
+    locale: string;
+  };
+}) {
+  return {
+    title:
+      params.locale === 'ru'
+        ? 'Услуги'
+        : params.locale === 'en'
+        ? 'Services'
+        : 'Pakalpojumi',
+  };
+}
 
 export default function ElectricityServicesPage() {
   return (
--- src/app/[locale]/fossil-energy-services/page.tsx
+++ src/app/[locale]/fossil-energy-services/page.tsx
@@ -1,13 +1,25 @@
-import { Metadata } from 'next';
 import StatisticBanner from '@/components/banners/StatisticBanner';
 import ContactForm from '@/components/contactForm/ContactForm';
 import HeroSection from '@/components/sections/HeroSection';
 import ServicesSection from '@/components/sections/ServicesSection';
 import fossilServicesData from '@/data/fossilServicesData.json';
 
-export const metadata: Metadata = {
-  title: 'Pakalpojumi',
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: {
+    locale: string;
+  };
+}) {
+  return {
+    title:
+      params.locale === 'ru'
+        ? 'Услуги'
+        : params.locale === 'en'
+        ? 'Services'
+        : 'Pakalpojumi',
+  };
+}
 
 export default function FossilServicesPage() {
   return (
